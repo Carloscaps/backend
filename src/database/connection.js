e4eaa4b9c61a4b17242b3720d779083649f52ad3
andrea <andreitaramirez94@gmail.com>
2021-01-19 13:34:28 -0300

    Conexión a base de datos modificada

--- src/database/connection.js
+++ src/database/connection.js
@@ -2,9 +2,11 @@ export const config = {
     user: 'sa',
     password: 'sqlserver2014',
     server: 'localhost',
-    database: 'Wilug',
+    database: 'db_Wilug',
     options: {
+        trustedconnection: true,
         enableArithAbort: true,
-        encrypt: true
+        instancename: 'SQLEXPRESS' //Se puede omitir
     },
+    port : 51688
 };
