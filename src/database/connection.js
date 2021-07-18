8303ade5ba62ed2c62541abd3185613a81b56ecb
carlos myrus <carlosmyrus@gmail.com>
2021-07-17 23:16:37 -0400

    test mail

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,8 +1,8 @@
 export const config = {
-    user: 'dbwilug',
-    password: '',
+    user: 'dbtest',
+    password: 'Wilug21#',
     server: 'wcorpd.wilug.cl',
-    database: 'WilugCorpD',
+    database: 'WilugCorpD_Test',
     options: {
         trustedconnection: true,
         enableArithAbort: true,
